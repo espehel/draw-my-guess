e5b86f2d30bff9d9499e7b32868e49535700886c
My Thao Nguyen <mythng94@gmail.com>
2020-04-30 21:22:25 +0200

    Save and showcase saved drawings

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import './less/main.less';
-import App from './components/App';
+import App from './App';
 import { GameProvider } from './state/GameContext';
 
 const rootElement = document.querySelector('#root');
