aa793cc1c41fe913477bd9fe763646c43c4aec53
Espen Hellerud <espen.hellerud@bekk.no>
2020-05-09 16:09:13 +0200

    Add styling to CreateSpace

--- src/app/components/CreateSpace.tsx
+++ src/app/components/CreateSpace.tsx
@@ -1,13 +1,38 @@
 import React, { FC, useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
 import { Connection } from '../api/Connection';
 import { createSpace } from '../api/space';
 import { useConnectToSpace, useSpace } from '../state/SpaceContext';
+import { Typography } from '@material-ui/core';
 
+const useStyles = makeStyles({
+  main: {
+    textAlign: 'center',
+  },
+  header: {
+    padding: '0.3em',
+  },
+  create: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: '3em',
+    alignItems: 'center',
+  },
+  textField: {
+    width: '100%',
+    maxWidth: '30em',
+    padding: '1em',
+  },
+});
 const CreateSpace: FC = () => {
   const [connection, setConnection] = useState<Connection>();
   const [nickname, setNickname] = useState('');
   const connectToSpace = useConnectToSpace();
   const { setPlayer } = useSpace();
+  const classes = useStyles();
 
   useEffect(() => {
     setConnection(Connection.setupConnection('/'));
@@ -18,22 +43,38 @@ const CreateSpace: FC = () => {
   }
 
   return (
-    <article>
-      <h1>Draw my Guess</h1>
-      <p>Set a nickname</p>
-      <input onChange={(event) => setNickname(event.target.value)} />
-      <button
-        onClick={async () => {
-          const space = await createSpace({
-            hostName: nickname,
-            hostId: connection.socket.id,
-          });
-          setPlayer({ id: space.host.id, name: nickname });
-          connectToSpace(`/${space.id}`);
-        }}
-      >
-        Create game
-      </button>
+    <article className={classes.main}>
+      <Typography className={classes.header} variant="h1">
+        Draw my Guess
+      </Typography>
+      <Typography variant="subtitle1">
+        A game where drawings and guesses are passed around for hysterical
+        results.
+      </Typography>
+      <section className={classes.create}>
+        <Typography>Set a nickname</Typography>
+        <TextField
+          className={classes.textField}
+          placeholder="Nickname"
+          onChange={(event) => setNickname(event.target.value)}
+          variant="outlined"
+        ></TextField>
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          onClick={async () => {
+            const space = await createSpace({
+              hostName: nickname,
+              hostId: connection.socket.id,
+            });
+            setPlayer({ id: space.host.id, name: nickname });
+            connectToSpace(`/${space.id}`);
+          }}
+        >
+          Create game
+        </Button>
+      </section>
     </article>
   );
 };
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 import './less/main.less';
 import App from './App';
@@ -7,9 +8,21 @@ import { SpaceProvider } from './state/SpaceContext';
 
 const rootElement = document.querySelector('#root');
 
+const theme = createMuiTheme({
+  overrides: {
+    MuiTypography: {
+      h1: {
+        fontSize: '3rem',
+      },
+    },
+  },
+});
+
 render(
-  <SpaceProvider>
-    <App />
-  </SpaceProvider>,
+  <ThemeProvider theme={theme}>
+    <SpaceProvider>
+      <App />
+    </SpaceProvider>
+  </ThemeProvider>,
   rootElement
 );
